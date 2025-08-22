7842b1c705618c2813786eb545f41c1d3f441678
unknown <mr7aali@gmail.com>
2025-08-22 23:53:42 +0600

    update admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,8 @@ import AdminDashboard from "./AdminDashboard";
 
 const fetchUsers = async () => {
   const res = await fetch(`${process.env.BACKEND_URL}/api/v1/users`, {
-    next: { tags: ["users"] },
+    next: { tags: ["users"], revalidate: 0 },
+    cache: "no-store",
   });
   if (!res.ok) throw new Error("Failed to fetch users");
   return res.json();
@@ -10,7 +11,8 @@ const fetchUsers = async () => {
 
 const fetchProperties = async () => {
   const res = await fetch(`${process.env.BACKEND_URL}/api/v1/property`, {
-    next: { tags: ["property"] },
+    next: { tags: ["property"], revalidate: 0 },
+    cache: "no-store",
   });
   if (!res.ok) throw new Error("Failed to fetch properties");
   return res.json();
@@ -19,7 +21,7 @@ const fetchProperties = async () => {
 const fetchNeedApproveProperties = async () => {
   const res = await fetch(
     `${process.env.BACKEND_URL}/api/v1/property/not-approved`,
-    { next: { tags: ["property"] } }
+    { next: { tags: ["property"], revalidate: 0 }, cache: "no-store" }
   );
   if (!res.ok) throw new Error("Failed to fetch properties needing approval");
   return res.json();
